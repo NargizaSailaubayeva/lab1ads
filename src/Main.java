82dd1987e6827e37d5ce2654357612cab7573f06
Nargiza <nargizok04s@gmail.com>
2023-04-05 22:13:57 +0500

    lab1p6

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        int n = 5;
+        int n = 2;
+        int m = 10;
 
-        System.out.println(Fibonacci(n));
+        System.out.println(powerOfNum(n,m));
     }
-    public static int Fibonacci(int n) {
-        if(n==0){
-            return 0; //if n=0 it returns 0
-        }
-        else if(n==1){
-            return 1; // if n=1 it returns 1
+    public static int powerOfNum(int n, int m) {
+        if(m==0){
+            return 1; //if counter m=0, it returns 1
         }
         else{
-            return Fibonacci(n-1)+Fibonacci(n-2); //in recursive case, return sum of functions
+            return powerOfNum(n,m-1)*n; //in recursive case, it returns n*recursion
         }
     }
 }
